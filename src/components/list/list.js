e79c5be88c3a7a725b2cfb724da98585322b2616
ohsnaplol <emyb@bellsouth.net>
2018-06-18 21:51:15 -0400

    added list component

---
+++ src/components/list/list.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class List extends React.Component {
+
+  deleteItem(index) {
+    this.props.deleteItem(index)
+  }
+
+  itemRender() {
+    return (
+      this.props.items.map((item, index) => 
+        <li className="list-group-item" key={index}>
+          {item}
+          <button className="btn btn-danger" onClick={() => this.deleteItem(index)}>X</button>
+        </li>
+      )
+    )
+  }
+
+  render() {
+    const items = this.props
+
+    return (
+      <ul className="list-group">
+        {this.itemRender(items)}
+      </ul>
+    )
+  }
+}
+
+export default List
